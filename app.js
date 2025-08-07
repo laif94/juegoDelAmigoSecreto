d135bf0a3381701ce79c5c92be351e847e731e2b
Leo Ibarra <70456265+laif94@users.noreply.github.com>
2025-08-07 00:17:40 -0400

    Primera versión funcional "Juego del Amigo Secreto"
    Las principales funcionalidades agregadas incluyen:
    
    Adición de amigos:
    - Permite a los usuarios agregar nombres a una lista.
    - Valida que el campo de texto no esté vacío.
    - Valida y previene la adición de nombres duplicados.
    - Capitaliza automáticamente la primera letra de cada nombre para mantener un formato consistente.
    Sorteo de amigos:
    - Implementa un algoritmo para seleccionar un nombre de la lista de forma aleatoria.
    - Muestra el nombre del amigo secreto sorteado en la interfaz de usuario.
    - Valida que la lista contenga al menos dos nombres antes de realizar el sorteo.
    Mejoras en la interfaz de usuario:
    - Actualiza dinámicamente la lista de amigos en la página a medida que se añaden nuevos nombres.
    - Se optimizó el código para mostrar los mensajes de resultado de manera más eficiente, utilizando la función cambiarTexto().

--- app.js
+++ app.js
@@ -1,1 +1,69 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//VARIABLES
+
+let listaDeAmigos = [];
+let listaHTML = document.getElementById('listaAmigos');
+//FUNCIONES
+
+//Cambiar texto de resultado según parámetro.
+function cambiarTexto(nuevoTexto) {
+    let listaAmigos = document.getElementById('resultado');
+    listaAmigos.textContent = nuevoTexto;
+}
+
+//Agregar un amigo a la lista y mostrar la lista actualizada.
+function agregarAmigo() {
+   let valorUsuario = document.getElementById('amigo').value;
+   let nombreUsuario = valorUsuario.trim(); // Se eliminan espacios antes y después del texto
+   // Se capitaliza la primera letra del nombre
+   nombreUsuario = nombreUsuario.toLowerCase().charAt(0).toUpperCase() + nombreUsuario.toLowerCase().slice(1);
+
+   //Validación de valor vacío
+   if (nombreUsuario != '') {
+      if(listaDeAmigos.includes(nombreUsuario)) {
+         alert('No se permiten nombres repetidos');
+      } else {
+         listaDeAmigos.push(nombreUsuario); //Registro del nombre en el array
+         document.getElementById('amigo').value = '';
+         mostrarAmigos();
+      }
+   } else {
+      alert('Por favor, ingrese un nombre');
+   }
+   console.log(nombreUsuario);
+   console.log(listaDeAmigos);
+}
+
+function mostrarAmigos() {
+   
+   //Limpiar estante
+   listaHTML.innerHTML = '';
+
+   //recorrer el array listaDeAmigos 
+   for (let i = 0; i < listaDeAmigos.length; ++i) {
+      //En cada vuelta crear un libro nuevo
+      let amigoItem = document.createElement('li');
+
+      //Escribir el nombre del amigo en el libro
+      amigoItem.textContent = listaDeAmigos[i];
+
+      //Colocar texto en el estante
+      listaHTML.appendChild(amigoItem);
+   }
+}
+
+
+function sortearAmigo() {
+   let indiceAleatorio = 0;
+   if (listaDeAmigos.length < 2) {
+      alert('Ingrese al menos 2 nombres en la lista ');
+   } else {
+      indiceAleatorio = Math.floor(Math.random()*listaDeAmigos.length);
+   }
+   console.log(indiceAleatorio)
+   listaHTML.innerHTML = '';
+   cambiarTexto(`El ganador es ${listaDeAmigos[indiceAleatorio]}`)
+}
+//<h2 class="section-title">Digite el nombre de sus amigos</h2>
+//    <ul id="listaAmigos" class="name-list" aria-labelledby="listaAmigos" role="list"></ul>
+//    <ul id="resultado" class="result-list" aria-live="polite"></ul>
